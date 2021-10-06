69d91e179bfe4aadd8372fd19f6d5aee01134522
Jacky Tea <teajacks@outlook.com>
2021-10-06 16:12:51 -0400

    render list of coins

--- JTea_DPS926_Assignment2/MainPage.xaml.cs
+++ JTea_DPS926_Assignment2/MainPage.xaml.cs
@@ -4,13 +4,17 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace JTea_DPS926_Assignment2
 {
     public partial class MainPage : ContentPage
     {
-        NetworkingService service = new NetworkingService();
+        public NetworkingService service = new NetworkingService();
+
+        public ObservableCollection<Coin> coins { get; private set; }
+
         public MainPage()
         {
             InitializeComponent();
@@ -18,7 +22,29 @@ namespace JTea_DPS926_Assignment2
 
         protected async override void OnAppearing()
         {
+            coins = new ObservableCollection<Coin>();
+            var coinsData = await service.getAllCoins();
+            foreach (Coin c in coinsData)
+            {
+                coins.Add(
+                    new Coin(
+                        c.id,
+                        c.symbol,
+                        c.name,
+                        c.image,
+                        c.price,
+                        new Description("")
+                    )
+                );
+            }
+            loadingCoins.IsRunning = false;
+            listOfCoins.ItemsSource = coins;
             base.OnAppearing();
+        }
+
+        private void listOfCoins_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+
         }
     }
 }
